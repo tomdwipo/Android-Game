296c26a4566bbc3f823c71c6c93f4f39cca56f40
Mohit <mohit110891@gmail.com>
2015-06-24 15:37:06 +0530

    temp added

--- src/com/example/game/TempSprite.java
+++ src/com/example/game/TempSprite.java
@@ -3,6 +3,7 @@ package com.example.game;
 import java.util.List;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
 public class TempSprite {
 	
@@ -14,7 +15,20 @@ public class TempSprite {
 	
 	public TempSprite(List<TempSprite> temp,GameView view,float x,float y,Bitmap bit) {
 		this.x = Math.min(Math.max(x-bit.getWidth()/2,0), view.getWidth() - bit.getWidth());
+		this.y = Math.min(Math.max(y-bit.getHeight()/2,0), view.getHeight() - bit.getHeight());
+		this.bit = bit;
+		this.temp =temp;
 		
+	}
+	
+	public void onDraw(Canvas canvas){
+		update();
+		canvas.drawBitmap(bit, x, y,null);
+	}
+	public void update(){
+		if(--life < 1){
+			temp.remove(life);
+		}
 	}
 
 }
